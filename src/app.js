971c745ea49c0cbad94567974884972110959fe1
LuthGom <lucianomendes23@yahoo.com>
2024-02-27 00:54:10 -0300

    feat:handling returns statements

--- src/app.js
+++ src/app.js
@@ -7,14 +7,25 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-await db.connect()
+await db
+  .connect()
   .then((conn) => {
     console.log("The db  connected succefully!");
+    return conn;
   })
-  .catch((err) => console.log("The db connection failed!" + err));
+  .catch((err) => {
+    console.log("The db connection failed!" + err);
+    return err;
+  });
 db.query(createTables)
-  .then((result) => console.log("Tables created!"))
-  .catch((err) => console.log(err));
+  .then((result) => {
+    console.log("Tables created!");
+    return result;
+  })
+  .catch((err) => {
+    console.log(err);
+    return err;
+  });
 app.use((req, res, next) => {
   next();
 });
